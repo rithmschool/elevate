c4a4e82b80d0927631bc6b962ee0b55a7fc77060
FAYCEL TOUILI <fayceltouili@gmail.com>
2019-08-21 11:43:03 -0700

    Landing page. installed reactstrap bootstrap writing tests

---
+++ frontend/src/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './Home.css';
+import img1 from './img/001.jpg';
+
+class Home extends React.Component {
+
+
+  render(){
+    
+    return( 
+      <div >
+        <h1 className="Home-h1"> Are You paid what you deserve?</h1>
+        <img src={img1} className="Home-stretch" alt="image1" />
+      </div>
+    );
+  }
+  
+}
+
+export default Home;
