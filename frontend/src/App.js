08d1e27248e939df9d04249646a40c346cb3ba82
FAYCEL TOUILI <fayceltouili@gmail.com>
2019-08-21 12:07:18 -0700

    navigation bar added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { Component } from "react";
+import Navigation from "./Navigation";
 import Routes from "./Routes";
 import './App.css'
 
-class App extends React.Component {
+class App extends Component {
 
   render(){
     return(
       <div className="App">
+        <Navigation />
         <Routes/>
        </div>
     )
---
+++ frontend/src/Navigation.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import './Navigation.css'
+import { Collapse, Navbar, NavbarToggler} from 'reactstrap';
+
+export default class Navigation extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+/** Navbar appears when user is logged in */
+
+  loggedInNavbar(){
+    return(
+      <Navbar color="light" light expand="md">
+        <Link className="navbar-brand Nav-text" to="/">Super Agent</Link>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+          <ul className="navbar-nav mr-auto mt-2 mt-lg-0 ">
+            <li className="nav-item ">
+              <Link to="/link1" className="Nav-link Nav-link-ltr">Our Mission</Link>
+            </li>
+            <li className="nav-item">
+            <Link to="/link2" className="ml-5 Nav-link Nav-link-ltr ">How it Works</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/link3" className="ml-5 Nav-text Nav-link Nav-link-ltr">Explore</Link>
+            </li>
+            </ul>
+          <ul className="nav justify-content-end">
+            <li className="nav-item">
+              <Link to="/link3" className="mr-5 Nav-link Nav-link-ltr">Help</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/link3" className="fas fa-user Nav-icon"></Link>
+            </li>
+          </ul>   
+        </Collapse>
+      </Navbar>
+    );
+                        
+  }
+/** Navbar appears when user is logged out */
+  loggedOutNavbar(){
+    return(
+      <Navbar color="light" light expand="md">
+        <Link className="navbar-brand Nav-text" to="/">Super Agent</Link>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+          <ul className="navbar-nav mr-auto mt-2 mt-lg-0 ">
+            <li className="nav-item ">
+              <Link to="/link1" className="Nav-link Nav-link-ltr">Get Started</Link>
+            </li>
+            <li className="nav-item">
+            <Link to="/link2" className="ml-5 Nav-link Nav-link-ltr ">How it Works</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/link3" className="ml-5 Nav-text Nav-link Nav-link-ltr">About</Link>
+            </li>
+            </ul>
+          <ul className="nav justify-content-end">
+            <li className="nav-item">
+              <Link to="/link3" className="mr-5 Nav-link Nav-link-ltr">Help</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/link3" className="mr-5 Nav-link Nav-link-ltr"> Sign In</Link>
+            </li>
+          </ul>   
+        </Collapse>
+      </Navbar>
+    );
+  }
+  render() {
+    return (
+      this.loggedInNavbar()
+    )
+  }
+}
