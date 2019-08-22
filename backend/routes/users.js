4ab4bce47075f14be9c71186840b4ec880b1df2b
Stephanie Simms <stephanie.simms@gmail.com>
2019-08-22 14:56:12 -0700

    removed expressError helper function since not needed, updated users routes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,7 +1,6 @@
 /** Routes for users. */
 
 const express = require('express');
-const ExpressError = require('../helpers/ExpressError');
 const { ensureCorrectUser, authRequired, adminRequired } = require('../middleware/auth');
 const User = require('../models/User');
 const createToken = require('./createToken');
@@ -19,27 +18,21 @@ router.get('/', authRequired, async function (req, res, next) {
   }
 });
 
-/** GET /[username] => {user: user} */
+/** GET / a specific user => {user: user} */
 
-router.get('/:username', authRequired, async function (req, res, next) {
+router.get('/', authRequired, async function (req, res, next) {
   try {
-    const user = await User.findOne(req.params.username);
+    const user = await User.findOne(req.body.user_id);
     return res.json({ user });
   } catch (err) {
     return next(err);
   }
 });
 
-/** POST / {userdata}  => {token: token} */
+/** POST / a new user {userdata}  => {token: token} */
 
 router.post('/', async function (req, res, next) {
   try {
-    const validation = validate(req.body, userNewSchema);
-
-    if (!validation.valid) {
-      throw new ExpressError(validation.errors.map(e => e.stack), 400);
-    }
-
     const newUser = await User.register(req.body);
     const token = createToken(newUser);
     return res.status(201).json({ token });
@@ -48,33 +41,22 @@ router.post('/', async function (req, res, next) {
   }
 });
 
-/** PATCH /[handle] {userData} => {user: updatedUser} */
+/** PATCH / a specific user {userData} => {user: updatedUser} */
 
-router.patch('/:username', ensureCorrectUser, async function (req, res, next) {
+router.patch('/', ensureCorrectUser, async function (req, res, next) {
   try {
-    if ('username' in req.body || 'is_admin' in req.body) {
-      throw new ExpressError(
-        'You are not allowed to change username or is_admin properties.',
-        400);
-    }
-
-    const validation = validate(req.body, userUpdateSchema);
-    if (!validation.valid) {
-      throw new ExpressError(validation.errors.map(e => e.stack), 400);
-    }
-
-    const user = await User.update(req.params.username, req.body);
+    const user = await User.update(req.body.user_id, req.body);
     return res.json({ user });
   } catch (err) {
     return next(err);
   }
 });
 
-/** DELETE /[handle]  =>  {message: "User deleted"}  */
+/** DELETE /  =>  {message: "User deleted"}  */
 
-router.delete('/:username', ensureCorrectUser, async function (req, res, next) {
+router.delete('/', ensureCorrectUser, async function (req, res, next) {
   try {
-    await User.remove(req.params.username);
+    await User.remove(req.body.user_id);
     return res.json({ message: 'User deleted' });
   } catch (err) {
     return next(err);
