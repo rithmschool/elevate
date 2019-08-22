768f8878d9d293c349fa151410876db0eff5be62
FAYCEL TOUILI <fayceltouili@gmail.com>
2019-08-22 10:49:30 -0700

    cleaning App.css, removing extra spaces

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Navigation from "./Navigation";
 import Routes from "./Routes";
-import './App.css'
 
 class App extends React.Component {
 
--- frontend/src/Navigation.js
+++ frontend/src/Navigation.js
@@ -81,7 +81,7 @@ class Navigation extends React.Component {
   }
   render() {
     return (
-      this.loggedOutNavbar()
+      this.loggedInNavbar()
     )
   }
 }
