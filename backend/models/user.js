c928c07a0b9648b6efa339ce3ddb56c2652492d1
Stephanie Simms <stephanie.simms@gmail.com>
2019-08-22 14:50:17 -0700

    minor formatting fixes and created users routes file WIP

--- backend/models/user.js
+++ backend/models/user.js
@@ -10,13 +10,13 @@ const BCRYPT_WORK_FACTOR = 10;
 
 class User {
 
-    /** authenticate user with email, password. Returns user or throws err. */
+  /** authenticate user with email, password. Returns user or throws err. */
 
-    static async authenticate(data) {
-        // try to find the user first
-       
-        const result = await db.query(
-            `SELECT id, 
+  static async authenticate(data) {
+    // try to find the user first
+
+    const result = await db.query(
+      `SELECT id, 
                 email,
                 password, 
                 is_admin,
@@ -28,61 +28,56 @@ class User {
                 goals     
           FROM users 
           WHERE email = $1`,
-            [data.email]
-        );
+      [data.email]
+    );
 
-        const user = result.rows[0];
-        
-      
-       
-        if (user) {
-            // compare hashed password to a new hash from password from user input
-            const isValid = await bcrypt.compare(data.password, user.password);
-         
-            if (isValid) {
-               
-                return user;
-            }
-        }
+    const user = result.rows[0];
 
-        const invalidPass = new Error("Invalid Credentials");
-        invalidPass.status = 401;
-        throw invalidPass;
+
+
+    if (user) {
+      // compare hashed password to a new hash from password from user input
+      const isValid = await bcrypt.compare(data.password, user.password);
+
+      if (isValid) {
+
+        return user;
+      }
     }
 
-    /** Register user with data. Returns new user data. */
-    /**NOTE: ask Alex what kind of initial sign up data from new user */
-    static async register(data) {
-        // check if email is taken or not
-        const duplicateCheck = await db.query(
-            `SELECT email 
+    const invalidPass = new Error("Invalid Credentials");
+    invalidPass.status = 401;
+    throw invalidPass;
+  }
+
+  /** Register user with data. Returns new user data. */
+  /**NOTE: ask Alex what kind of initial sign up data from new user */
+  static async register(data) {
+    // check if email is taken or not
+    const duplicateCheck = await db.query(
+      `SELECT email 
             FROM users 
             WHERE email = $1`,
-            [data.email]
-        );
+      [data.email]
+    );
 
-        if (duplicateCheck.rows[0]) {
-            const err = new Error(
-                `There already exists a user with email '${data.email}`);
-            err.status = 401;
-            throw err;
-        }
+    if (duplicateCheck.rows[0]) {
+      const err = new Error(
+        `There already exists a user with email '${data.email}`);
+      err.status = 401;
+      throw err;
+    }
 
-        const hashedPassword = await bcrypt.hash(data.password, BCRYPT_WORK_FACTOR);
-        
-        const result = await db.query(
-            `INSERT INTO users 
+    const hashedPassword = await bcrypt.hash(data.password, BCRYPT_WORK_FACTOR);
+
+    const result = await db.query(
+      `INSERT INTO users 
             (email, password) 
             VALUES ($1, $2) 
             RETURNING id, is_admin`,
-            [
-                data.email,
-                hashedPassword
-            ]);
-        return result.rows[0];
-    }
-
-
+      [data.email, hashedPassword]);
+    return result.rows[0];
+  }
 }
 
 
