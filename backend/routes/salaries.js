6161081b00543007caa81bfdf56a35094bf088ca
Stephanie Simms <stephanie.simms@gmail.com>
2019-08-25 17:49:59 -0700

    Fix syntax preventing successful compilation

--- backend/routes/salaries.js
+++ backend/routes/salaries.js
@@ -32,8 +32,6 @@ router.post('/', async function (req, res, next) {
 router.patch('/', async function (req, res, next) {
   try {
     //write logic to look up salary id from user_id fed in from request
-    const id = 
-
     const salary = await Salary.update(id, req.body);
     return res.json({ salary });
   } catch (err) {
