e3e54f22394d1552f9aefd506f6728ccaa0f5d2f
FAYCEL TOUILI <fayceltouili@gmail.com>
2019-08-26 16:32:39 -0700

    user profile wip

--- frontend/src/UserProfile.js
+++ frontend/src/UserProfile.js
@@ -1,70 +1,37 @@
 import React from 'react';
-import SubMenu from './SubMenu';
 import { NavItem, NavLink, Nav } from 'reactstrap';
-import classNames from 'classnames';
 import {Link} from 'react-router-dom';
 
-const UserProfile = props => (
-    <div className={classNames('sidebar', {'is-open': props.isOpen})}>
-      <div className="sidebar-header">
-        <span color="info" onClick={props.toggle} style={{color: '#fff'}}>&times;</span>
-        <h3>Bootstrap Sidebar</h3>
-      </div>
-      <div className="side-menu">
-        <Nav vertical className="list-unstyled pb-3">
-          <SubMenu title="Home" items={submenus[0]}/>
-          <NavItem>
-            <NavLink tag={Link} to={'/about'}>
-              About
-            </NavLink>
-          </NavItem>
-          <SubMenu title="Pages"  items={submenus[1]}/>
-          <NavItem>
-            <NavLink tag={Link} to={'/pages'}>
-              Portfolio
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink tag={Link} to={'/faq'}>
-              FAQ
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink tag={Link} to={'/contact'}>
-              Contact
-            </NavLink>
-          </NavItem>
-        </Nav>        
-      </div>
-    </div>
-  );
 
-  const submenus = [
-    [
-      {
-        title: "Home 1",
-        target: "Home-1"
-      },
-      {
-        title: "Home 2",
-        target: "Home-2",        
-      },
-      {
-        itle: "Home 3",
-        target: "Home-3",      
-      }
-    ],
-    [
-      {
-        title: "Page 1",
-        target: "Page-1",          
-      },
-      {
-        title: "Page 2",
-        target: "Page-2",        
-      }
-    ]
-  ]
-  
+class UserProfile extends React.Component {
+  state = { activeItem: 'My Comp Overview' }
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render() {
+    const { activeItem } = this.state
+
+    return (
+    <div className="row">
+      <Nav vertical>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">
+          <Link to="/#" activeClassName="selected">
+            My Comp overview
+          </Link>
+          </li>
+          <li class="list-group-item">
+            <Link to="#">My Offers</Link>
+          </li>
+          <li class="list-group-item">
+            <Link to="#">Upload an offer</Link>
+          </li>
+        </ul>
+      </Nav>
+    </div>
+      
+    )
+  }
+}
 
 export default UserProfile;
