483e9ace4076a1966d3f0c50fa4adc1927e757e5
Marisa Vatanya <mvatanya@gmail.com>
2019-09-03 14:06:30 -0700

    cleaned up AdminPanel

--- frontend/src/AdminPanel.js
+++ frontend/src/AdminPanel.js
@@ -80,7 +80,12 @@ class AdminPanel extends Component {
                         getUserDetail={ this.getUserDetail }
                         view={ this.state.view } /> : null
           }
-          {this.state.view === 'userDetail' ? <AdminUserView user={this.state.userDetail} updateUserState={this.updateUserState} updateViewState={this.updateViewState} /> : null }
+          {this.state.view === 'userDetail' 
+                                ? <AdminUserView 
+                                  user={this.state.userDetail} 
+                                  updateUserState={this.updateUserState} 
+                                  updateViewState={this.updateViewState} /> 
+                                : null }
         </div>
         
         <div className="admin-navbar">
