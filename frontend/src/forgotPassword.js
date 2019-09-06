9be45d6a4faae51e179fcb4abecc093f80541cc4
Elie Schoppik <eschoppik@gmail.com>
2019-09-05 23:06:05 -0700

    fixing naming

--- frontend/src/forgotPassword.js
+++ frontend/src/forgotPassword.js
@@ -1,107 +1,104 @@
-import React from 'react';
-import ElevateApi from './elevateApi';
-import { Col, Button, Form, Label, Input, Row} from 'reactstrap';
+import React from "react";
+import ElevateApi from "./elevateApi";
+import { Col, Button, Form, Label, Input, Row } from "reactstrap";
 import Alert from "./alert";
-import './ForgotPassword.css';
-import Spinner from './spinner';
-
-
+import "./ForgotPassword.css";
+import Spinner from "./spinner";
 
 class ForgotPassword extends React.Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      email: '',
+      email: "",
       errors: [],
       msgFromServer: [],
       emailSent: false,
-      isLoading: false,
-    }
+      isLoading: false
+    };
     this.handleChange = this.handleChange.bind(this);
     this.sendEmail = this.sendEmail.bind(this);
   }
 
   // handle input change
-  handleChange(evt){
-		this.setState({
+  handleChange(evt) {
+    this.setState({
       [evt.target.name]: evt.target.value,
-       emailSent: false,
-       errors: [],
-      });
+      emailSent: false,
+      errors: []
+    });
   }
 
-  
-
-
   // verify if user entred its email and send  it to backend and then handle response
-  async sendEmail(evt){
+  async sendEmail(evt) {
     evt.preventDefault();
-    if(this.state.email === ''){
-      this.setState({ errors: ['Please enter your email address!']});
-    } 
-    else {
-      try{
-        this.setState({ isLoading: true});
-        let response = await ElevateApi.forgotPassword({ email: this.state.email });
+    if (this.state.email === "") {
+      this.setState({ errors: ["Please enter your email address!"] });
+    } else {
+      try {
+        this.setState({ isLoading: true });
+        let response = await ElevateApi.forgotPassword({
+          email: this.state.email
+        });
         this.setState({
           errors: [],
           emailSent: true,
           msgFromServer: [response.message],
-          isLoading: false,
+          isLoading: false
         });
-      }
-      catch(errors){
+      } catch (errors) {
         this.setState({ errors, isLoading: false });
       }
     }
   }
 
-  render(){
-    const { email, errors, emailSent, isLoading} = this.state;
+  render() {
+    const { email, errors, emailSent, isLoading } = this.state;
 
-    if(isLoading ) return <Spinner />
+    if (isLoading) return <Spinner />;
 
-    return(
+    return (
       <div className=" container col-md-6 offset-md-3 col-lg-4 offset-lg-4 border rounded shadow ForgotPassword-container">
-      <Form onSubmit={this.sendEmail}>
-        <div style={{textAlign: 'center'}}>
-          <h3 style={{marginTop: '2%'}}>Forgot Password</h3>
-         <p>Please enter your email address and we'll send you instructions on how to reset your password</p>
-        </div>
-          <hr></hr><br></br>
-        <Row form>
-          <Col md={12}>                  
-            <Label className="form-group has-float-label">
-              <Input onChange={this.handleChange}
-                    value={email}
-                    type="email"
-                    name="email"
-                    id="forgt-email"
-                    />
-              <span>Email</span>
-            </Label>
+        <Form onSubmit={this.sendEmail}>
+          <div style={{ textAlign: "center" }}>
+            <h3 style={{ marginTop: "2%" }}>Forgot Password</h3>
+            <p>
+              Please enter your email address and we'll send you instructions on
+              how to reset your password
+            </p>
+          </div>
+          <hr></hr>
+          <br></br>
+          <Row form>
+            <Col md={12}>
+              <Label className="form-group has-float-label">
+                <Input
+                  onChange={this.handleChange}
+                  value={email}
+                  type="email"
+                  name="email"
+                  id="forgt-email"
+                />
+                <span>Email</span>
+              </Label>
             </Col>
           </Row>
           <hr></hr>
 
-          {errors.length > 0 &&
-              <Alert type="danger" messages={errors} />}
+          {errors.length > 0 && <Alert type="danger" messages={errors} />}
 
-            { emailSent &&
-              <Alert type="success"
-                    messages={this.state.msgFromServer} />}
+          {emailSent && (
+            <Alert type="success" messages={this.state.msgFromServer} />
+          )}
 
-          <Col align="center" >
-            <Button size="sm"
-            className="btn btn-info"
-              > 
-               Send Password Recovery Email</Button>
+          <Col align="center">
+            <Button size="sm" className="btn btn-info">
+              Send Password Recovery Email
+            </Button>
           </Col>
           <br></br>
         </Form>
       </div>
-    )
+    );
   }
 }
 export default ForgotPassword;
