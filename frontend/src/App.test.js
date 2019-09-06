97c76f487be51a9fb7da976ed21228efc5e92141
Elie Schoppik <eschoppik@gmail.com>
2019-09-05 23:22:58 -0700

    prettier

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import App from './App';
+import React from "react";
+import { shallow } from "enzyme";
+import App from "./App";
 
 // TODO: MAKE SURE THIS WORKS WITH CI
-xit('renders without crashing', function () {
+xit("renders without crashing", function() {
   shallow(<App />);
 });
