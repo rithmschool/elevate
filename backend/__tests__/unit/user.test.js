bef470d77ae4f5ac96128b217400d36bed7f85a5
Emi Tsukuda <gioramlevi13@gmail.com>
2019-09-06 21:40:28 -0700

    finished refactoring user.test.js

--- backend/__tests__/unit/user.test.js
+++ backend/__tests__/unit/user.test.js
@@ -4,77 +4,76 @@ process.env.NODE_ENV = "test";
 // model import
 const User = require("../../models/user")
 
-// test data imports 
+/// test data imports 
 const [TEST_USER_DATA] = require('../../seedData')
-const TEST_USER = TEST_USER_DATA[0]
+// getting one user data
+const TEST_USER_DATA_ONE = TEST_USER_DATA[0]
 
 //test config  
 const {
   afterAllHook,
-  beforeEachHook,
+  beforeAllHook,
 } = require('../configTest');
 
-describe("model user", function () {
-  beforeEach(async function () {
-    await beforeEachHook()
+beforeAll(async function () {
+  await beforeAllHook();
+});
+
+describe("Test User Class register", function () {
+  test("should return  id and is_admin after successful register", async function () {
+    let user = await User.create({
+      "email": "newuser@gmail.com",
+      "password": "secret",
+      "first_name": "new",
+      "last_name": "user"
+    })
+    expect(typeof user.id).toBe('number');
+    expect(user.is_admin).toEqual(false);
   });
 
-  describe("Test User Class register", function () {
-    test("should return  id and is_admin after successful register", async function () {
-      let user = await User.create({
-        "email": "newuser@gmail.com",
+  test("should return error message when registering duplicate email address", async function () {
+    try {
+      await User.create({
+        "email": "testuser@gmail.com",
         "password": "secret",
-        "first_name": "new",
-        "last_name": "user"
+        "first_name": "Test",
+        "last_name": "User"
       })
-      expect(typeof user.id).toBe('number');
-      expect(user.is_admin).toEqual(false);
-    });
+    } catch (err) {
+      expect(err.message).toEqual("There already exists a user with email 'testuser@gmail.com");
+      expect(err.status).toBe(401)
+    }
+  });
+});
 
-    test("should return error message when registering duplicate email address", async function () {
-      try {
-        await User.create({
-          "email": "testuser@gmail.com",
-          "password": "secret",
-          "first_name": "Test",
-          "last_name": "User"
-        })
-      } catch (err) {
-        expect(err.message).toEqual("There already exists a user with email 'testuser@gmail.com");
-        expect(err.status).toBe(401)
-      }
-    });
+describe("Test User Class authenticate", function () {
+  test("should return user object if correct password and email", async function () {
+    let user = await User.authenticate({ "email": TEST_USER_DATA_ONE.email, "password": TEST_USER_DATA_ONE.password })
+
+    expect(typeof user.id).toBe('number');
+    expect(user.email).toBe(TEST_USER_DATA_ONE.email);
+    expect(user.is_admin).toBe(false);
   });
 
-  describe("Test User Class authenticate", function () {
-    test("should return user object if correct password and email", async function () {
-      let user = await User.authenticate({ "email": TEST_USER.email, "password": TEST_USER.password })
-
-      expect(typeof user.id).toBe('number');
-      expect(user.email).toBe(TEST_USER.email);
-      expect(user.is_admin).toBe(false);
-    });
-
-    test("should return error message if password is wrong", async function () {
-      try {
-        await User.authenticate({ "email": TEST_USER.email, "password": "badPassword" })
-      } catch (err) {
-        expect(err.message).toEqual("Invalid Credentials");
-        expect(err.status).toBe(401)
-      }
-    });
-
-    test("should return error message if email is wrong", async function () {
-      try {
-        await User.authenticate({ "email": "bad@test.com", "password": TEST_USER.password })
-      } catch (err) {
-        expect(err.message).toEqual("Invalid Credentials");
-        expect(err.status).toBe(401)
-      }
-    });
+  test("should return error message if password is wrong", async function () {
+    try {
+      await User.authenticate({ "email": TEST_USER_DATA_ONE.email, "password": "badPassword" })
+    } catch (err) {
+      expect(err.message).toEqual("Invalid Credentials");
+      expect(err.status).toBe(401)
+    }
   });
 
-  afterAll(async function () {
-    await afterAllHook();
+  test("should return error message if email is wrong", async function () {
+    try {
+      await User.authenticate({ "email": "bad@test.com", "password": TEST_USER_DATA_ONE.password })
+    } catch (err) {
+      expect(err.message).toEqual("Invalid Credentials");
+      expect(err.status).toBe(401)
+    }
   });
-})
+});
+
+afterAll(async function () {
+  await afterAllHook();
+});
