40a10d9af3b9b5de635c77801573bf8be8675ff1
Nalipp <nateplipp@gmail.com>
2019-09-10 08:04:06 -0700

    setup admin login

--- backend/data.sql
+++ backend/data.sql
@@ -96,7 +96,7 @@ CREATE TABLE users_calendly_users (
 
 INSERT INTO users ( email, password, is_admin, first_name, last_name, current_company, hire_date, needs, goals) VALUES
   ('testuser@gmail.com', 'password123', false, 'Test', 'User', 'Google', '2018-06-23', 'Talk to financial advisor about salary/equity negotiations.', 'Increase in equity.'),
-  ('admin@gmail.com', 'admin123', true, 'Admin', 'User', '', '2019-06-23', '', ''),
+  ('admin@gmail.com', '$2b$15$JhwIcOCoqA4YRTIe6Ceh8OO4o8t9RCgr/mQ2TP0eL9JY8/si46HIW', true, 'Admin', 'User', '', '2019-06-23', '', ''),
   ('nate@gmail.com', 'nate123', false, 'Nate', 'Lipp', 'Rithm', '2019-06-23', 'Get help from a lawyer.', 'Increase in salary.'),
   ('elie@gmail.com', 'elie123', false, 'Elie', 'Schoppik', 'Rithm', '2017-06-01', 'Talk to financial advisor to calculate how many instructors he can hire.', 'Recruit more instructors.'),
   ('joel@gmail.com', 'joel123', false, 'Joel', 'Burton', 'Rithm', '2017-08-23', 'General investment advice', 'Help bootcamp grads negotiate.');
