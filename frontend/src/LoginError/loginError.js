d4d94e3987561f33ecdd28c7d845f207f2173b5b
Nalipp <nateplipp@gmail.com>
2019-09-16 16:41:44 -0700

    clean up 'LoginError' folder

--- frontend/src/LoginError/loginError.js
+++ frontend/src/LoginError/loginError.js
@@ -2,15 +2,14 @@ import React from "react";
 
 class LoginError extends React.Component {
   render() {
+
     return (
       <div style={{ textAlign: "center" }}>
         <h5>
-          {" "}
-          Invalid Email or Password <br></br>
+          Invalid Email or Password 
           <p>Forgot your password?</p>
           <a href="http://localhost:3000/reset-password/forgot"> click here.</a>
         </h5>
-        <hr></hr>
       </div>
     );
   }
