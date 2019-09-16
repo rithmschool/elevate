6dba9150c4b8202eb36c4d74b3747d285233f743
Nalipp <nateplipp@gmail.com>
2019-09-16 09:45:10 -0700

    move app files into shared folder

--- frontend/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { shallow } from "enzyme";
-import App from "./App";
-
-// TODO: MAKE SURE THIS WORKS WITH CI
-xit("renders without crashing", function() {
-  shallow(<App />);
-});
