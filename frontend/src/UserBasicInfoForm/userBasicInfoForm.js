28eecf6a778abff1eb137944c262d9051e4bd680
Nalipp <nateplipp@gmail.com>
2019-09-17 11:07:34 -0700

    clean up 'UserBasicInfoForm' folder

--- frontend/src/UserBasicInfoForm/userBasicInfoForm.js
+++ frontend/src/UserBasicInfoForm/userBasicInfoForm.js
@@ -5,6 +5,10 @@ import '../FormStyles.css';
 
 /** Update user basic info */
 class UserBasicInfoForm extends React.Component {
+  static defaultProps = {
+    current_company: '',
+  };
+
   constructor(props){
     super(props);
 
@@ -12,7 +16,7 @@ class UserBasicInfoForm extends React.Component {
       first_name: this.props.first_name,
       last_name: this.props.last_name,
       email: this.props.email,
-      current_company: this.props.current_company || '',
+      current_company: this.props.current_company,
       hire_date: moment(this.props.hire_date).format('YYYY-MM-DD'),
       isEdit: false,
     }
@@ -42,7 +46,7 @@ class UserBasicInfoForm extends React.Component {
     }
 
     this.props.handleUserUpdate(updatedUser, this.props.userId);
-    this.setState({isEdit: false})
+    this.setState({ isEdit: false })
   }
   
   render() {
@@ -50,107 +54,101 @@ class UserBasicInfoForm extends React.Component {
 
     return (
       <div
-        className={`container borderrounded shadow`}
-        style={{ margin: "10% auto", backgroundColor: "#F4F6F8" }}
-      >
+        className={ `container borderrounded shadow` }
+        style={{ margin: "10% auto", backgroundColor: "#F4F6F8" }}>
 
         <div className="form-inside-container mt-5">
           <Form onSubmit={ this.handleSubmit }> 
             <div className="form-styles_flex-space-between">
               <h3>Basic info</h3>
 
-              {!isEdit &&
+              { !isEdit &&
                 <i className="m-3 fas fa-edit fa-1x"
-                  onClick={this.toggleEditForm}>
-                </i>}
+                  onClick={ this.toggleEditForm }>
+                </i> }
             </div>
 
             <Form.Group>
               <span>First name</span>
 
               <Form.Control
-                onChange={this.handleChange}
+                onChange={ this.handleChange }
                 id="EditUser-first_name"
                 name="first_name"
                 type="text"
-                disabled={!isEdit}  
-                value={this.state.first_name}
-              />
+                disabled={ !isEdit }
+                value={ this.state.first_name } />
             </Form.Group>
 
             <Form.Group>
               <span>Last name</span>
 
               <Form.Control
-                onChange={this.handleChange}
+                onChange={ this.handleChange }
                 id="EditUser-last_name"
                 name="last_name"
                 type="text"
-                disabled={!isEdit}  
-                value={this.state.last_name}
-              />
+                disabled={ !isEdit }
+                value={ this.state.last_name } />
             </Form.Group>
 
             <Form.Group>
               <span>Email</span>
 
               <Form.Control
-                onChange={this.handleChange}
+                onChange={ this.handleChange }
                 id="EditUser-email"
                 name="email"
                 type="text"
-                disabled={!isEdit}  
-                value={this.state.email}
-              />
+                disabled={ !isEdit }
+                value={ this.state.email } />
             </Form.Group>
 
             <Form.Group>
               <span>Current company</span>
 
               <Form.Control
-                onChange={this.handleChange}
+                onChange={ this.handleChange }
                 id="EditUser-current_company"
                 name="current_company"
                 type="text"
-                disabled={!isEdit}  
-                value={this.state.current_company}
-              />
+                disabled={ !isEdit }
+                value={ this.state.current_company } />
             </Form.Group>
 
             <Form.Group>
               <span>Hire date</span>
 
               <Form.Control
-                onChange={this.handleChange}
+                onChange={ this.handleChange }
                 id="EditUser-hire_date"
                 name="hire_date"
                 type="date"
-                disabled={!isEdit}  
-                value={this.state.hire_date}
-              />
+                disabled={ !isEdit }
+                value={ this.state.hire_date } />
             </Form.Group>
 
             <div className="row justify-content-center">
-              {isEdit &&
-                <div>
-                  <Button
-                    className="login-submit mr-3 ml-3"
-                    type="submit">
-                    Submit
-                  </Button>
+              { isEdit &&
+                  <div>
+                    <Button
+                      className="login-submit mr-3 ml-3"
+                      type="submit">
+                      Submit
+                    </Button>
 
-                  <h6 
-                    className="mr-3 ml-3 form-styles_cancel"
-                    onClick={this.toggleEditForm}>
-                    Cancel
-                  </h6>
-                </div>}
+                    <h6 
+                      className="mr-3 ml-3 form-styles_cancel"
+                      onClick={this.toggleEditForm}>
+                      Cancel
+                    </h6>
+                  </div> }
             </div>
 
           </Form>
         </div>
       </div>
-    )
+    );
   }
 }
 
