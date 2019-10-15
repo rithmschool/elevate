40f6592fd4d537a05e88ff4841261cfb96be472d
Mac Glass <macglass19@gmail.com>
2019-10-15 16:53:41 -0700

    Redid NavBar
    Had to redo to resolve merge conflict that was unusual. Just updated the link names and logo

--- frontend/src/App/Navigation/navigation.js
+++ frontend/src/App/Navigation/navigation.js
@@ -1,151 +1,175 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import classNames from "classnames";
-import "./navigation.css";
-import { Collapse, Navbar, NavbarToggler } from "reactstrap";
-import { UserContext, AdminContext } from "../../userContext";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import classNames from 'classnames';
+import './navigation.css'
+import { Collapse, Navbar, NavbarToggler } from 'reactstrap';
+import { UserContext, AdminContext } from "./../userContext";
 
-class Navigation extends React.Component {
-  constructor(props) {
-    super(props);
-    this.userMenuToggle = this.userMenuToggle.bind(this);
-    this.toggle = this.toggle.bind(this);
-    this.handleClickOutside = this.handleClickOutside.bind(this);
 
-    this.state = {
-      isOpen: false,
-      userMenuIsOpen: false
-    };
-  }
+class Navigation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.userMenuToggle = this.userMenuToggle.bind(this);
+    this.toggle = this.toggle.bind(this);
+    this.handleClickOutside = this.handleClickOutside.bind(this);
 
-  componentDidMount() {
-    document.addEventListener("mousedown", this.handleClickOutside);
-  }
+    this.state = {
+      isOpen: false,
+      userMenuIsOpen: false
+    };
+  }
 
-  componentWillUnmount() {
-    document.removeEventListener("mousedown", this.handleClickOutside);
-  }
+  componentDidMount() {
+    document.addEventListener("mousedown", this.handleClickOutside);
+  }
 
-  myRef = React.createRef();
+  componentWillUnmount() {
+    document.removeEventListener("mousedown", this.handleClickOutside);
+  }
 
-  // Hide user menu when click outside dropdown
-  handleClickOutside = e => {
-    if (!this.myRef.current.contains(e.target)) {
-      this.setState({ userMenuIsOpen: false });
-    }
-  };
+  myRef = React.createRef();
 
-  toggle() {
-    this.setState(st => ({ isOpen: !st.isOpen }));
-  }
+  // Hide user menu when click outside dropdown
+  handleClickOutside = e => {
+    if (!this.myRef.current.contains(e.target)) {
+      this.setState({ userMenuIsOpen: false });
+    }
+  };
 
-  userMenuToggle(evt) {
-    this.setState(st => ({ userMenuIsOpen: !st.userMenuIsOpen }));
-  }
+  toggle() {
+    this.setState(st => ({ isOpen: !st.isOpen }));
+  }
 
-  render() {
-    // check if user is connected to display wether sign in or logout on navbar
-    const userIsLoggedIn = (
-      <UserContext.Consumer>
-        {currentUser => (
-          <li className="nav-item active">
-            {currentUser ? (
-              <div>
-                <i
-                  className="fas fa-user Nav-icon"
-                  onClick={this.userMenuToggle}
-                ></i>
+  userMenuToggle(evt) {
+    this.setState(st => ({ userMenuIsOpen: !st.userMenuIsOpen }));
+  }
 
-                <div
-                  ref={this.myRef}
-                  className={classNames("userMenu", {
-                    "is-open": this.state.userMenuIsOpen
-                  })}
-                >
-                  <ul
-                    className="list-group list-group-flush"
-                    onClick={this.userMenuToggle}
-                  >
-                    <li className="list-group-item bg-transparent">
-                      <Link
-                        to={`users/${currentUser.userId}`}
-                        className="Menu-link "
-                      >
-                        Profile
-                      </Link>
-                    </li>
+  render() {
+    // check if user is connected to display wether sign in or logout on navbar
+    const userIsLoggedIn = (
+      <UserContext.Consumer>
+        {currentUser => (
+          <li className="nav-item active">
+            {currentUser
+              ?
+              <div>
+                <i className="fas fa-user Nav-icon"
+                  onClick={this.userMenuToggle}>
+                </i>
 
-                    <li className="list-group-item bg-transparent">
-                      <Link
-                        className="Menu-link "
-                        to="/"
-                        onClick={this.props.logout}
-                      >
-                        Log out
-                      </Link>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            ) : (
-              <div ref={this.myRef}>
-                <Link to="/login" className="Nav-link Nav-link-ltr">
-                  Sign In
-                </Link>
-              </div>
-            )}
-          </li>
-        )}
-      </UserContext.Consumer>
-    );
+                <div ref={this.myRef}
+                  className={classNames('userMenu',
+                    { 'is-open': this.state.userMenuIsOpen })}>
 
-    const userIsAdmin = (
-      <AdminContext.Consumer>
-        {isAdmin =>
-          isAdmin && (
-            <li className="nav-item adminStyle danger active">
-              <Link className="Nav-text Nav-link Nav-link-ltr" to="/admin">
-                **Admin**
-              </Link>
-            </li>
-          )
-        }
-      </AdminContext.Consumer>
-    );
+                  <ul className="list-group list-group-flush"
+                    onClick={this.userMenuToggle}>
 
-    return (
-      <Navbar color="light" light expand="md">
-        <Link className="Nav_brand-name" to="/">
-          Elevate
-        </Link>
+                    <li className="list-group-item bg-transparent">
+                      <Link to={`users/${currentUser.userId}`}
+                        className="Menu-link ">
+                        Profile
+                          </Link>
+                    </li>
 
-        <NavbarToggler onClick={this.toggle} />
+                    <li className="list-group-item bg-transparent">
+                      <Link className="Menu-link "
+                        to="/"
+                        onClick={this.props.logout}>
+                        Log out
+                          </Link>
+                    </li>
 
-        <Collapse
-          isOpen={this.state.isOpen}
-          navbar
-          className="collapse.navbar-collapse"
-        >
-          <ul className="navbar-nav mr-auto">
-            <li
-              className="nav-item active"
-              data-toggle="collapse"
-              data-target=".in"
-            >
-              <Link to="/ask-an-expert" className="Nav-link Nav-link-ltr">
-                Ask An Expert
-              </Link>
-            </li>
-          </ul>
-          <ul className="navbar-nav">
-            {userIsAdmin}
+                  </ul>
+                </div>
+              </div>
+              :
+              <div ref={this.myRef}>
+                <Link to="/login"
+                  className="Nav-link Nav-link-ltr">
+                  Sign In
+                    </Link>
+              </div>
+            }
+          </li>
+        )}
+      </UserContext.Consumer>
+    );
 
-            {userIsLoggedIn}
-          </ul>
-        </Collapse>
-      </Navbar>
-    );
-  }
+    const userIsAdmin = (
+      <AdminContext.Consumer>
+        {isAdmin => (
+          isAdmin &&
+          <li className="nav-item adminStyle danger active">
+            <Link className="Nav-text Nav-link Nav-link-ltr"
+              to="/admin">
+              **Admin**
+              </Link>
+          </li>
+        )}
+      </AdminContext.Consumer>
+    );
+
+    return (
+      <Navbar color="purple" light expand="md">
+        <Link className="Nav_brand-name" to="/">
+          Brella
+        </Link>
+
+        <NavbarToggler onClick={this.toggle} />
+
+        <Collapse
+          isOpen={this.state.isOpen}
+          navbar
+          className="collapse.navbar-collapse">
+
+          <ul className="navbar-nav mr-auto">
+            <li
+              className="nav-item active"
+              data-toggle="collapse"
+              data-target=".in">
+
+              <Link to="/"
+                className="Nav-link Nav-link-ltr">
+                How it works
+              </Link>
+            </li>
+
+            <li
+              className="nav-item active"
+              data-toggle="collapse"
+              data-target=".in">
+
+              <Link to="/"
+                className="Nav-link Nav-link-ltr">
+                About
+              </Link>
+            </li>
+          </ul>
+
+          <ul className="navbar-nav ml-auto">
+            <li
+              className="nav-item active"
+              data-toggle="collapse"
+              data-target=".in">
+
+              <Link to="/"
+                className="Nav-link Nav-link-ltr">
+                For Employeers
+              </Link>
+            </li>
+
+          </ul>
+          <ul className="navbar-nav">
+
+            {userIsAdmin}
+
+            {userIsLoggedIn}
+
+          </ul>
+        </Collapse>
+      </Navbar>
+    );
+  }
 }
 
-export default Navigation;
+export default Navigation;
