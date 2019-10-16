d86c84b807658262e10fb7b8164f3c996eaf8301
Mac Glass <macglass19@gmail.com>
2019-10-16 16:46:15 -0700

    WIP Updated Navigation Testing
    WIP Added multiple tests for the Nav Bar when the user is not logged in including testing the logo link, the lifecylce functions. Once the routes and components have been added, and we have determined how the navbar will look for logged in users and admins, we can finish writing tests.

--- frontend/src/App/Navigation/Navigation.test.js
+++ frontend/src/App/Navigation/Navigation.test.js
@@ -2,31 +2,27 @@ import React from "react";
 import { shallow, mount } from "enzyme";
 import { Link } from "react-router-dom";
 import { MemoryRouter } from "react-router";
-import Home from '../Routes/Home/home'
+import Home from "../Routes/Home/home";
 import toJson from "enzyme-to-json";
 import Navigation from "./navigation";
 import App from "../../App/app";
 
-
-//TODO After Components and Routes have been added, giving the NavBar full functionality: 
+//TODO After Components and Routes have been added, giving the NavBar full functionality:
 // 1. Finish writing Tests without user logged in (Links that haven't been added, see note below)
-// 2. Write tests for when User is logged in and when Admin is logged in (NavBar yet to be determined) 
+// 2. Write tests for when User is logged in and when Admin is logged in (NavBar yet to be determined)
 
-describe("Nav Bar Testing Without Logged In User", function () {
-
-  it("renders without crashing", function () {
+describe("Nav Bar Testing Without Logged In User", function() {
+  it("renders without crashing", function() {
     shallow(<Navigation />);
   });
 
-
-  it("matches snapshot", function () {
+  it("matches snapshot", function() {
     let wrapper = shallow(<Navigation />);
     let serialized = toJson(wrapper);
     expect(serialized).toMatchSnapshot();
   });
 
-
-  it("renders Navbar component on mount", function () {
+  it("renders Navbar component on mount", function() {
     let wrapper = mount(
       <MemoryRouter>
         <App />
@@ -35,53 +31,57 @@ describe("Nav Bar Testing Without Logged In User", function () {
     expect(wrapper.find("Navbar")).toHaveLength(1);
   });
 
-//TODO Recreate this test for other buttons when those components have been made 
+  //TODO Recreate this test for other buttons when those components have been made
   it("Make sure Logo Link exists, and click it to make sure it take you Home", () => {
     const wrapper = mount(
       <MemoryRouter>
         <App />
       </MemoryRouter>
     );
-    expect(wrapper.contains(<Link className="Nav_brand-name" to="/">
-      Brella
-  </Link>)).toBe(true)
-    wrapper.find(Link).first().simulate('click');
+    expect(
+      wrapper.contains(
+        <Link className="Nav_brand-name" to="/">
+          Brella
+        </Link>
+      )
+    ).toBe(true);
+    wrapper
+      .find(Link)
+      .first()
+      .simulate("click");
     expect(wrapper.find(Home)).toHaveLength(1);
   });
 
-  it("makes sure to run componentDidMount", function () {
+  it("makes sure to run componentDidMount", function() {
     const mockComponentDidMount = jest.spyOn(
       Navigation.prototype,
-      'componentDidMount'
-    )
+      "componentDidMount"
+    );
     mount(
       <MemoryRouter>
         <Navigation />
       </MemoryRouter>
     );
-    expect(mockComponentDidMount).toHaveBeenCalled()
+    expect(mockComponentDidMount).toHaveBeenCalled();
     expect(mockComponentDidMount).toHaveBeenCalledTimes(1);
   });
 
-
-  it("runs componentDidUnmount when unmounted", function () {
+  it("runs componentDidUnmount when unmounted", function() {
     const mockComponentDidUnmount = jest.spyOn(
       Navigation.prototype,
-      'componentWillUnmount'
-    )
+      "componentWillUnmount"
+    );
     const wrapper = mount(
       <MemoryRouter>
         <Navigation />
       </MemoryRouter>
     );
-    wrapper.unmount()
-    expect(mockComponentDidUnmount).toHaveBeenCalled()
+    wrapper.unmount();
+    expect(mockComponentDidUnmount).toHaveBeenCalled();
     expect(mockComponentDidUnmount).toHaveBeenCalledTimes(1);
   });
 
-
-  it("expect three links on unlogged in Nav Bar", function () {
-
+  it("expect three links on unlogged in Nav Bar", function() {
     let wrapper = mount(
       <MemoryRouter>
         <Navigation />
@@ -89,5 +89,4 @@ describe("Nav Bar Testing Without Logged In User", function () {
     );
     expect(wrapper.find(Link).length).toBe(3);
   });
-
 });
