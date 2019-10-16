dad2f035e71499b2b9fd47f734588199c6e08a5c
timothy egart <timothyegart@Justins-MBP-3.localdomain>
2019-10-16 14:12:09 -0700

    Created dashboard routes and set up dashboard components

---
+++ frontend/src/App/Routes/dashboardRoutes.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+
+/** if user isnt connected can't access to Dashboard routes */
+class DashboardRoutes extends React.Component {
+  render() {
+    const token = localStorage.getItem("token");
+
+    if (!token) return <Redirect to='/login' />;
+
+    return (
+      <Route
+        exact={this.props.exact}
+        path={this.props.path}
+        render={this.props.render}
+      />
+    );
+  }
+}
+
+export default DashboardRoutes;
--- frontend/src/App/Routes/routes.js
+++ frontend/src/App/Routes/routes.js
@@ -8,20 +8,25 @@ import UserProfile from "./UserProfile/userProfile";
 import LoginSignUpForm from "./LoginSignUpForm/loginSignUpForm";
 import AdminPanel from "./AdminPanel/adminPanel";
 import AskAnExpert from "./AskAnExpert/askAnExpert";
+import Dashboard from "./../Routes/Dashboard/dashboard";
+import DashboardAppointments from "./../Routes/Dashboard/dashboardAppointments";
+import DashboardManage from "./../Routes/Dashboard/dashboardManage";
+import DashboardTemplates from "./../Routes/Dashboard/dashboardTemplates";
 
 import AdminPrivateRoute from "./adminPrivateRoute";
 import ForgotPassRoutes from "./forgotPassRoutes";
 import UserPrivateRoute from "./userPrivateRoute";
+import DashboardRoutes from "./dashboardRoutes";
 
 class Routes extends React.Component {
   render() {
     return (
       <Switch>
-        <Route exact path="/" render={props => <Home {...props} />} />
+        <Route exact path='/' render={props => <Home {...props} />} />
 
         <Route
           exact
-          path="/login"
+          path='/login'
           render={props => (
             <LoginSignUpForm
               {...props}
@@ -32,30 +37,47 @@ class Routes extends React.Component {
 
         <AdminPrivateRoute
           exact
-          path="/admin"
+          path='/admin'
           render={props => <AdminPanel {...props} />}
         />
 
-        <Route exact path="/ask-an-expert" render={() => <AskAnExpert />} />
+        <Route exact path='/ask-an-expert' render={() => <AskAnExpert />} />
 
         <UserPrivateRoute
-          path="/users/:userId"
+          path='/users/:userId'
           render={props => <UserProfile {...props} />}
+        />
+
+        <DashboardRoutes exact path='/dashboard' render={() => <Dashboard />} />
+        <DashboardRoutes
+          exact
+          path='/dashboard/manage'
+          render={() => <DashboardManage />}
+        />
+        <DashboardRoutes
+          exact
+          path='/dashboard/appointments'
+          render={() => <DashboardAppointments />}
+        />
+        <DashboardRoutes
+          exact
+          path='/dashboard/templates'
+          render={() => <DashboardTemplates />}
         />
 
         <ForgotPassRoutes
           exact
-          path="/reset-password/forgot"
+          path='/reset-password/forgot'
           render={() => <ForgotPassword />}
         />
 
         <ForgotPassRoutes
           exact
-          path="/reset-password/:token"
+          path='/reset-password/:token'
           render={props => <ResetPassword {...props} />}
         />
 
-        <Redirect to="/" />
+        <Redirect to='/' />
       </Switch>
     );
   }
