ea71d527c1229af4cd4e7aa70c7c9a5e32bf5333
Kimberly Johnson <kajohnson3140@gmail.com>
2019-10-16 11:00:25 -0700

    updated branch

---
+++ frontend/src/App/Routes/AdminPanel/AdminUserView/adminUserView.test 2.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { mount } from "enzyme";
+import toJson from "enzyme-to-json";
+
+import AdminUserView from "./adminUserView";
+
+describe("AdminUserView", function() {
+  let wrapper;
+  const testData = {
+    first_name: "Han",
+    last_name: "Solo",
+    email: "han@solo.com",
+    current_company: "SpaceX",
+    hire_date: "2018-09-01",
+    needs: "Negotiate a raise during annual review",
+    goals: "Increase salary by 15k"
+  };
+
+  beforeEach(() => {
+    wrapper = mount(<AdminUserView user={testData} />);
+  });
+
+  it("renders without crashing", function() {
+    // Mounted in beforeEach above
+  });
+
+  it("matches snapshot", function() {
+    const serialized = toJson(wrapper);
+
+    expect(serialized).toMatchSnapshot();
+  });
+
+  it("has a user prop", function() {
+    expect(wrapper.prop("user")).toEqual(testData);
+  });
+
+  it("has div with adminUserView_div class", function() {
+    expect(wrapper.find("div.adminUserView_div")).toHaveLength(1);
+  });
+
+  it("has user info fields", function() {
+    expect(wrapper.html()).toContain("Email");
+    expect(wrapper.html()).toContain("Company");
+    expect(wrapper.html()).toContain("Hire Date");
+    expect(wrapper.html()).toContain("Needs");
+    expect(wrapper.html()).toContain("Goals");
+    expect(wrapper.html()).toContain("Questions");
+  });
+});
---
+++ frontend/src/App/Routes/Home/home.test 2.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { shallow } from "enzyme";
+import toJson from "enzyme-to-json";
+import Home from "./home";
+
+it("renders without crashing", function() {
+  shallow(<Home />);
+});
+
+it("matches snapshot", function() {
+  let wrapper = shallow(<Home />);
+  let serialized = toJson(wrapper);
+  expect(serialized).toMatchSnapshot();
+});
