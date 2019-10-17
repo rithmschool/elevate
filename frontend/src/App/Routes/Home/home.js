c35ce8a9fbe80003966931cabfbbd3d6ca4dcd99
WinnieGotYu <WinnieGotYu@gmail.com>
2019-10-17 15:33:54 -0700

    Added images and texts to .Home_database

--- frontend/src/App/Routes/Home/home.js
+++ frontend/src/App/Routes/Home/home.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Button } from "reactstrap";
 import { Link } from "react-router-dom";
 import { Container, Row, Col } from "reactstrap";
-import Image from "react-bootstrap/Image";
 
 import "./home.css";
 import img1 from "./img/001.jpg";
@@ -26,12 +25,12 @@ class Home extends React.Component {
         <Container fluid className="Home_template">
           <Row>
             <Col md={6}>
-              <div className="Home_template_child">
+              <div>
                 <img className="card-img" src={img1} alt="working" />
               </div>
             </Col>
             <Col md={6}>
-              <div className="Home_template_child">
+              <div className="Home_template-intro">
                 <h1>
                   <b>Why Brella?</b>
                 </h1>
@@ -51,92 +50,50 @@ class Home extends React.Component {
         <Container fluid className="Home_database">
           <Row>
             <Col>
-            <h1 className="Home_database-title">How it works</h1>
+              <h1 className="Home_database-title">How it works</h1>
             </Col>
           </Row>
           <Row>
-            <Col xs={6} md={4}>
-              <p>
+            <Col md={4}>
+              <div className="Home_database-img">
+                <img src={file} alt="startFile" />
+              </div>
+              <div>
+                <p className="Home_database-text">
+                  Upload your documents to Brella. Upload your documents to
+                  Brella. Upload your documents to Brella. Upload your documents
+                  to Brella. Upload your documents to Brella. Upload your
+                  documents to Brella.
+                </p>
+              </div>
+            </Col>
+            <Col md={4}>
+              <div className="Home_database-img">
+                <img src={person} alt="person" />
+              </div>
+              <p className="Home_database-text">
                 Upload your documents to Brella. Upload your documents to
                 Brella. Upload your documents to Brella.
               </p>
             </Col>
-            <Col xs={6} md={4}>
-              <p>
-                Upload your documents to Brella. Upload your documents to
-                Brella. Upload your documents to Brella.
-              </p>
-            </Col>
-            <Col xs={6} md={4}>
-              <p>
+            <Col md={4}>
+              <div className="Home_database-img">
+                <img src={completed} alt="completed" />
+              </div>
+              <p className="Home_database-text">
                 Upload your documents to Brella. Upload your documents to
                 Brella. Upload your documents to Brella.
               </p>
             </Col>
           </Row>
         </Container>
-        {/* <div className="Home_database">
-          <div className="col-sm-4">
-            <img className="img-thumbnail mx-auto d-block" src={file} alt="image1"  />
-            <h3 className="text-center">Step 1</h3>
-            <p className="text">
-              Upload your documents to Brella.
-              Upload your documents to Brella.
-              Upload your documents to Brella.
-            </p>
-          </div>
-       
-          <div className="col-sm-4">
-            <img className="img-thumbnail mx-auto d-block" src={person} alt="image1"  />
-            <h3 className="text-center">Step 2</h3>
-            <p className="text">
-              Upload your documents to Brella.
-              Upload your documents to Brella.
-              Upload your documents to Brella.
-            </p>
-          </div>
-       
-          <div className="col-sm-4">
-            <img className="img-thumbnail mx-auto d-block" src={completed} alt="image1"  />
-            <h3 className="text-center">Step 3</h3>
-            <p className="text">
-              Upload your documents to Brella.
-              Upload your documents to Brella.
-              Upload your documents to Brella.
-            </p>
-          </div>
-       
-        </div> */}
 
-        {/* <div className="Home_stats">
-          <div className="row">
-            <div className="col-sm-4">
-              <div className="Home_circle ">
-                <p>75% increase offer</p>
-              </div>
-            </div>
-
-            <div className="col-sm-4">
-              <div className="Home_circle ">
-                <p>20% average increase in salary</p>
-              </div>
-            </div>
-
-            <div className="col-sm-4">
-              <div className="Home_circle ">
-                <p>90% better understand comp</p>
-              </div>
-            </div>
-          </div>
-    </div> */}
-
-        {/* <div className="Home_get-started">
-          <h2 className="">Get Started Today</h2>
-
-          <Link to="/ask-an-expert">
-            <Button color="primary">Ask An Expert</Button>
+        <div className="Home_get-started">
+          <h1 className="">Make Use Of Brella Today</h1>
+          <Link to={routeStr} className="btn btn-primary">
+            Get Started
           </Link>
-        </div> */}
+        </div>
       </div>
     );
   }
