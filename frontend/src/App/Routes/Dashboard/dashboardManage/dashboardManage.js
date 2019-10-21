28464dae5bd0e4cccdca675d990f8158c2a3da98
WinnieGotYu <WinnieGotYu@gmail.com>
2019-10-21 16:12:54 -0700

    Created folder for dashboardManage

---
+++ frontend/src/App/Routes/Dashboard/dashboardManage/dashboardManage.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import UserDocsDataTable from "./UserDocsDataTable";
+
+class DashboardManage extends Component {
+  render() {
+    return (
+      <div>
+        <UserDocsDataTable />
+      </div>
+    );
+  }
+}
+
+export default DashboardManage;
