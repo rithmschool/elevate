2d79551981f97f4a53383ea5e12de7790ec6d4e6
Nick Taylor <nst447@gmail.com>
2019-10-22 15:43:10 -0700

    R13 user forms hook refactor (#146)
    * Refactored UserBasicInfoForm to use hooks
    
    * Refactored UserBasicInfoForm to use hooks with testing
    
    * Added cancel button testing
    
    * Finished refactoring, and added tests, still working on adding more
    
    * Still working on adding additional tests
    
    * Finish creating tests for the UserSalaryInfo Form
    
    * Renamed initial state test to make more descriptive
    
    * Finished creating tests for the user salary form
    
    * Removed shallow from tests and imports
    
    * Made changes requested in PR review

--- frontend/src/App/Routes/UserProfile/UserSalaryInfoForm/userSalaryInfoForm.test.js
+++ frontend/src/App/Routes/UserProfile/UserSalaryInfoForm/userSalaryInfoForm.test.js
@@ -1,18 +1,9 @@
 import React from "react";
-import { shallow, mount } from "enzyme";
+import { mount } from "enzyme";
 import toJson from "enzyme-to-json";
 import UserSalaryInfoForm from "./userSalaryInfoForm";
-
-it("renders without crashing", function() {
-  shallow(<UserSalaryInfoForm />);
-});
-
-it("matches snapshot", function() {
-  let wrapper = shallow(<UserSalaryInfoForm />);
-  let serialized = toJson(wrapper);
-
-  expect(serialized).toMatchSnapshot();
-});
+import { Form } from "react-bootstrap";
+import { render, fireEvent } from "@testing-library/react";
 
 describe("UserSalaryInfoForm", function() {
   let wrapper;
@@ -24,20 +15,95 @@ describe("UserSalaryInfoForm", function() {
     equity: 0.1
   };
 
-  beforeEach(() => {
+  beforeEach(function() {
     wrapper = mount(<UserSalaryInfoForm {...latestSalary} />);
   });
 
-  it("has states", function() {
-    expect(wrapper.state("user_id")).toEqual(1);
-    expect(wrapper.state("salary")).toEqual(50000);
-    expect(wrapper.state("bonus")).toEqual(100);
-    expect(wrapper.state("equity")).toEqual(0.1);
-    expect(wrapper.state("isEdit")).toEqual(false);
+  it("renders without crashing", function() {
+    render(<UserSalaryInfoForm {...latestSalary} />);
   });
 
-  it("changes isEdit state to true whem click on edit", function() {
-    wrapper.find("i.fa-edit").simulate("click");
-    expect(wrapper.state("isEdit")).toEqual(true);
+  it("shows my default text", function() {
+    expect(wrapper.find("h3").text()).toEqual("Salary info");
+  });
+
+  it("matches snapshot", function() {
+    let serialized = toJson(wrapper);
+    expect(serialized).toMatchSnapshot();
+  });
+
+  it("renders all inputs", function() {
+    expect(wrapper.find(Form.Control)).toHaveLength(3);
+  });
+
+  it("has correct initial state", function() {
+    const { getByTestId } = render(<UserSalaryInfoForm {...latestSalary} />);
+    const bonusInput = getByTestId("bonus");
+    const salaryInput = getByTestId("salary");
+    const equityInput = getByTestId("equity");
+
+    expect(bonusInput.value).toBe("100");
+    expect(salaryInput.value).toBe("50000");
+    expect(equityInput.value).toBe("0.1");
+  });
+
+  it("changes values via the fireEvent.change method", function() {
+    const { getByTestId } = render(<UserSalaryInfoForm {...latestSalary} />);
+    const input = getByTestId("salary");
+    fireEvent.change(input, { target: { value: "100000" } });
+    expect(input.value).toBe("100000");
+    fireEvent.change(input, { target: { value: "999" } });
+    expect(input.value).toBe("999");
+  });
+
+  it("enables the form inputs when edit toggled on", function() {
+    const { getByTestId } = render(<UserSalaryInfoForm {...latestSalary} />);
+    const input = getByTestId("salary");
+    const toggle = getByTestId("turnEditOn");
+    fireEvent.click(toggle);
+    expect(input.disabled).toBeFalsy();
+  });
+
+  it("disables the form inputs on submission", function() {
+    const handleSalaryUpdate = jest.fn();
+    const { getByTestId } = render(
+      <UserSalaryInfoForm
+        {...latestSalary}
+        handleSalaryUpdate={handleSalaryUpdate}
+      />
+    );
+    const input = getByTestId("salary");
+    fireEvent.submit(input);
+    expect(input.disabled).toBeTruthy();
+  });
+
+  it("disables the form inputs upon clicking cancel", function() {
+    const handleSalaryUpdate = jest.fn();
+    const { getByTestId } = render(
+      <UserSalaryInfoForm
+        {...latestSalary}
+        handleSalaryUpdate={handleSalaryUpdate}
+      />
+    );
+    const toggle = getByTestId("turnEditOn");
+    const input = getByTestId("salary");
+    fireEvent.click(toggle);
+    expect(input.disabled).toBeFalsy();
+    const cancel = getByTestId("cancel");
+    fireEvent.click(cancel);
+    expect(input.disabled).toBeTruthy();
+  });
+
+  it("calls handleSalaryUpdate on submission", function() {
+    const handleSalaryUpdate = jest.fn();
+    const { getByTestId } = render(
+      <UserSalaryInfoForm
+        {...latestSalary}
+        handleSalaryUpdate={handleSalaryUpdate}
+      />
+    );
+    const input = getByTestId("salary");
+    fireEvent.submit(input);
+    expect(handleSalaryUpdate).toBeCalledTimes(1);
   });
 });
