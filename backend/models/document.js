2ed5cbca378233283812ff81993aaa803ff04d69
WinnieGotYu <WinnieGotYu@gmail.com>
2019-10-23 10:22:34 -0700

    Added date_submitted

--- backend/models/document.js
+++ backend/models/document.js
@@ -3,7 +3,7 @@ const db = require("../db");
 class Document {
   static async getAll(userId) {
     const result = await db.query(
-      `SELECT id, title, counterparty, url, date_reviewed, status
+      `SELECT id, title, counterparty, url, date_reviewed, date_submitted, status
     FROM documents
     WHERE user_id=$1`,
       [userId]
