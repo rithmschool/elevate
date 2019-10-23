aca3dbff20f52ad3e7e4051c26b9cd185dfb2a46
timothy egart <timothyegart@Justins-MBP-3.localdomain>
2019-10-23 09:37:13 -0700

    Fixed hard coded value in documents route

--- backend/routes/documents.js
+++ backend/routes/documents.js
@@ -8,7 +8,6 @@ const router = express.Router();
 
 // GET--- endpoint for getting all documents
 router.get("/manage", ensureCorrectUser, async function(req, res, next) {
-  
   try {
     let documents = await Document.getAll(req.user_id);
     return res.json({ documents });
