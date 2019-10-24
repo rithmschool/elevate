96f28198771d7e67a880a0ae113d1c6ca4e2784f
Mac Glass <macglass19@gmail.com>
2019-10-24 11:19:54 -0700

    refactored navigation component
    Refactored to have three consts for LoggedIn, LoggedOut and Admin. Makes it easier to style individually. Also changed to react bootstrap, and was able to fixthe issue where narrowing the screen would mess up the navbar. Moved the admin button to the drop down.

--- frontend/src/App/Navigation/navigation.js
+++ frontend/src/App/Navigation/navigation.js
@@ -2,8 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 import classNames from "classnames";
 import "./navigation.css";
-import { Collapse, Navbar, NavbarToggler } from "reactstrap";
+import { Collapse, NavbarToggler } from "reactstrap";
 import { UserContext } from "../../userContext";
+import { thisTypeAnnotation, thisExpression } from "@babel/types";
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
 
 class Navigation extends React.Component {
   constructor(props) {
@@ -18,11 +20,11 @@ class Navigation extends React.Component {
     };
   }
 
-  componentDidMount() {
+  async componentDidMount() {
     document.addEventListener("mousedown", this.handleClickOutside);
   }
 
-  componentWillUnmount() {
+  async componentWillUnmount() {
     document.removeEventListener("mousedown", this.handleClickOutside);
   }
   myRef = React.createRef();
@@ -43,108 +45,87 @@ class Navigation extends React.Component {
   }
 
   render() {
+
+    const loggedOut = (
+      <UserContext.Consumer>
+        {currentUser => (!currentUser) && (
+          <div ref={this.myRef}>
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item active" data-toggle="collapse" data-target=".in">
+              <Nav.Link href="/" className="Nav-link Nav-link-ltr">
+                  About
+              </Nav.Link>
+              </li>
+              <li className="nav-item active" data-toggle="collapse" data-target=".in">
+              <Nav.Link href="/" className="Nav-link Nav-link-ltr">
+                  For Employers
+              </Nav.Link>
+              </li>
+              <li className="nav-item active" data-toggle="collapse" data-target=".in">
+                <Nav.Link href="/login" className="Nav-link Nav-link-ltr">
+                  Sign In
+              </Nav.Link>
+              </li>
+            </ul>
+          </div>
+        )}
+      </UserContext.Consumer>
+    );
+
     const userIsLoggedIn = (
       <UserContext.Consumer>
-        {currentUser => (
-          <li className="nav-item active">
-            {currentUser ? (
-              <div>
-                <ul className="navbar-nav">
-                  <li
-                    className="nav-item active"
-                    data-toggle="collapse"
-                    data-target=".in"
-                  >
-                    <Link
-                      to="/dashboard/manage"
-                      className="Nav-link Nav-link-ltr"
-                    >
-                      Manage
-                    </Link>
-                  </li>
-                  <li
-                    className="nav-item active"
-                    data-toggle="collapse"
-                    data-target=".in"
-                  >
-                    <Link
-                      to="/dashboard/appointments"
-                      className="Nav-link Nav-link-ltr"
-                    >
-                      Appointments
-                    </Link>
-                    <Link
-                      to="/dashboard/templates"
-                      className="Nav-link Nav-link-ltr"
-                    >
-                      Templates
-                    </Link>
-                    <i
-                      className="fas fa-user Nav-icon right-content"
-                      onClick={this.userMenuToggle}
-                    ></i>
-                  </li>
-                </ul>
+        {currentUser => currentUser && (
+          <li className="nav-item active ">
+            <div>
+              <ul className="navbar-nav">
 
-                <div
-                  ref={this.myRef}
-                  className={classNames("userMenu", {
-                    "is-open": this.state.userMenuIsOpen
-                  })}
-                >
-                  <ul
-                    className="list-group list-group-flush"
-                    onClick={this.userMenuToggle}
-                  >
-                    <li className="list-group-item bg-transparent">
-                      <Link
-                        to={`users/${currentUser.userId}`}
-                        className="Menu-link "
-                      >
-                        Profile
-                      </Link>
-                    </li>
+                <li className="nav-item active user-item" data-toggle="collapse" data-target=".in">
+                  <Nav.Link
+                    href="/dashboard/manage" className="Nav-link Nav-link-ltr">
+                    Manage
+                  </Nav.Link>
+                </li>
 
-                    <li className="list-group-item bg-transparent">
-                      <Link
-                        className="Menu-link "
-                        to="/"
-                        onClick={this.props.logout}
-                      >
-                        Log out
-                      </Link>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            ) : (
-              <div ref={this.myRef}>
-                <ul className="navbar-nav ml-auto">
-                  <li
-                    className="nav-item active"
-                    data-toggle="collapse"
-                    data-target=".in"
-                  >
-                    <Link to="/" className="Nav-link Nav-link-ltr">
-                      About
-                    </Link>
+                <li className="nav-item active user-item" data-toggle="collapse" data-target=".in">
+                  <Nav.Link to="/dashboard/appointments" className="Nav-link Nav-link-ltr">
+                    Appointments
+                  </Nav.Link>
+                </li>
+
+                <li
+                  className="nav-item active user-item" data-toggle="collapse" data-target=".in">
+                  <Nav.Link href="/dashboard/templates" className="Nav-link Nav-link-ltr">
+                    Templates
+                  </Nav.Link>
+                </li>
+
+                <li className="nav-item active" data-toggle="collapse" data-target=".in">
+                  <i className="fas fa-user Nav-icon right-content" onClick={this.userMenuToggle}>
+                  </i>
+                </li>
+              </ul>
+              <div ref={this.myRef}
+                className={classNames("userMenu collasible-nav-dropdown", {
+                  "is-open collasible-nav-dropdown": this.state.userMenuIsOpen
+                })}
+              >
+                <ul className="list-group list-group-flush" onClick={this.userMenuToggle}>
+                  <li className="list-group-item bg-transparent">
+                    <NavDropdown.Item to={`users/${currentUser.userId}`} className="Menu-link">Profile</NavDropdown.Item>
                   </li>
-                  <li
-                    className="nav-item active"
-                    data-toggle="collapse"
-                    data-target=".in"
-                  >
-                    <Link to="/" className="Nav-link Nav-link-ltr">
-                      For Employers
-                    </Link>
 
-                    <Link to="/login" className="Nav-link Nav-link-ltr">
-                      Sign In
-                    </Link>
+                  <li className="list-group-item bg-transparent" data-toggle="collapse" data-target=".in">
+                    {userIsAdmin}
+                  </li>
+
+                  <li className="list-group-item bg-transparent">
+                    <NavDropdown.Item className="Menu-link" href="/" onClick={this.props.logout}>
+                      Log out
+                    </NavDropdown.Item>
                   </li>
                 </ul>
               </div>
-            )}
+            </div>
           </li>
         )}
       </UserContext.Consumer>
@@ -152,16 +133,11 @@ class Navigation extends React.Component {
 
     const userIsAdmin = (
       <UserContext.Consumer>
-        {currentUser =>
-          currentUser &&
-          currentUser.is_admin && (
-            <li className="nav-item adminStyle danger active">
-              <Link
-                className="Nav-text Nav-link Nav-link-ltr middlecontent"
-                to="/admin"
-              >
-                **Admin**
-              </Link>
+        {currentUser => currentUser && currentUser.is_admin && (
+            <li className="nav-item danger active">
+              <NavDropdown.Item className="Menu-link" href="/admin">
+                Admin
+              </NavDropdown.Item>
             </li>
           )
         }
@@ -169,24 +145,22 @@ class Navigation extends React.Component {
     );
 
     return (
-      <Navbar color="light" light expand="md">
-        <Link className="Nav_brand-name" to="/">
-          Brella
-        </Link>
+      <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+        <Navbar.Brand><Link className="Nav_brand-name" to="/"> Brella </Link></Navbar.Brand>
 
-        <NavbarToggler onClick={this.toggle} />
+        <Navbar.Toggle onClick={this.toggle} aria-controls="responsive-navbar-nav" />
 
-        <Collapse
-          isOpen={this.state.isOpen}
-          navbar
-          className="collapse.navbar-collapse"
+        <Navbar.Collapse id="responsive-navbar-nav" isOpen={this.state.isOpen} className="collapse.navbar-collapse"
         >
-          <ul className="navbar-nav ml-auto">
-            {userIsAdmin}
+          <Nav className="ml-auto" variant="light">
+            <ul className="navbar-nav justify-content-center">
 
-            {userIsLoggedIn}
-          </ul>
-        </Collapse>
+              {loggedOut}
+              {userIsLoggedIn}
+
+            </ul>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     );
   }
