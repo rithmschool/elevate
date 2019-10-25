4ec82f1e9f4222f51414e0d3ca04c609f56cbdd5
Nalipp <nateplipp@gmail.com>
2019-10-25 11:15:08 -0700

    create salary unit tests for findLatestSalaryByUserId

--- backend/__tests__/unit/salary.test.js
+++ backend/__tests__/unit/salary.test.js
@@ -5,29 +5,28 @@ const Salary = require("../../models/salary");
 const User = require("../../models/user");
 const { SEED_USER_SQL, SEED_SALARY_SQL } = require("../../config");
 
-
-describe("Test Salary model", function () {
+describe("Test Salary model", function() {
   beforeEach(async () => {
     await db.query(`DELETE FROM users;`);
-    await db.query(`ALTER SEQUENCE users_id_seq RESTART WITH 1;`)
+    await db.query(`ALTER SEQUENCE users_id_seq RESTART WITH 1;`);
     await db.query(`DELETE FROM salaries;`);
-    await db.query(`ALTER SEQUENCE salaries_id_seq RESTART WITH 1;`)
+    await db.query(`ALTER SEQUENCE salaries_id_seq RESTART WITH 1;`);
     await db.query(SEED_USER_SQL);
     await db.query(SEED_SALARY_SQL);
   });
 
   afterEach(async () => {
     await db.query(`DELETE FROM users;`);
-    await db.query(`ALTER SEQUENCE users_id_seq RESTART WITH 1;`)
+    await db.query(`ALTER SEQUENCE users_id_seq RESTART WITH 1;`);
     await db.query(`DELETE FROM salaries;`);
-    await db.query(`ALTER SEQUENCE salaries_id_seq RESTART WITH 1;`)
+    await db.query(`ALTER SEQUENCE salaries_id_seq RESTART WITH 1;`);
   });
 
   afterAll(async () => {
     await db.end();
   });
 
-  test("create a new salary", async function () {
+  test("create a new salary", async function() {
     const newUser = {
       email: "john@doe.com",
       password: "yeehaw",
@@ -41,32 +40,48 @@ describe("Test Salary model", function () {
     };
     const userResponse = await User.register(newUser);
 
-    const newSalary = { 
+    const newSalary = {
       user_id: userResponse.id,
-      salary: 105000.00, 
-      bonus: 2000.00, 
-      equity: 0.005 
+      salary: 105000.0,
+      bonus: 2000.0,
+      equity: 0.005
     };
 
     const response = await Salary.create(newSalary);
-    expect(response).toEqual({ 
+    expect(response).toEqual({
       id: expect.any(Number),
       user_id: expect.any(Number),
-      salary: 105000.00, 
-      bonus: 2000.00, 
-      equity: 0.005 });
+      salary: 105000.0,
+      bonus: 2000.0,
+      equity: 0.005
+    });
   });
 
-  test("delete a salary", async function () {
+  test("find latest salary for user by id", async function() {
+    const response = await Salary.findLatestSalaryByUserId(1);
+    const salary1 = { id: 1, user_id: 1, salary: 150000, bonus: 25000, equity: 0.001 }
+
+    expect(response).toEqual(salary1);
+  });
+
+  test("throws error if for salary retrievial if no user", async function() {
+    try {
+      await Salary.findLatestSalaryByUserId(100);
+    } catch(err) {
+      expect(err.message).toEqual("Cannot find a salary for user with userId 100");
+    }
+  });
+
+  test("delete a salary", async function() {
     const response = await Salary.remove(2);
     expect(response).toEqual({ id: 2 });
   });
 
-  test("throws error if salary to delete does not exist", async function () {
+  test("throws error if salary to delete does not exist", async function() {
     try {
-      await Salary.remove(6);
+      await Salary.remove(100);
     } catch (err) {
-      expect(err.message).toEqual('There exists no salary 6');
+      expect(err.message).toEqual("There exists no salary 100");
     }
   });
 });
--- backend/models/salary.js
+++ backend/models/salary.js
@@ -2,8 +2,8 @@ const db = require("../db");
 
 class Salary {
   static async findLatestSalaryByUserId(userId) {
-      const result = await db.query(
-        `SELECT 
+    const result = await db.query(
+      `SELECT 
             id,
             user_id,
             salary,
@@ -12,12 +12,14 @@ class Salary {
          FROM salaries
          WHERE user_id=$1
          ORDER BY id desc
-         LIMIT 1`, [userId]);
-      const salary = result.rows[0];
-      if (salary === undefined) {
-        throw new Error(`Cannot find a salary for user with userId ${userId}`);
-      }
-      return salary;
+         LIMIT 1`,
+      [userId]
+    );
+    const salary = result.rows[0];
+    if (salary === undefined) {
+      throw new Error(`Cannot find a salary for user with userId ${userId}`);
+    }
+    return salary;
   }
 
   static async create(data) {
@@ -29,7 +31,7 @@ class Salary {
       `INSERT INTO salaries (user_id, salary, bonus, equity) 
              VALUES ($1, $2, $3, $4) 
              RETURNING id, user_id, salary, bonus, equity`,
-            [user_id, salary, bonus, equity]
+      [user_id, salary, bonus, equity]
     );
     return result.rows[0];
   }
@@ -39,7 +41,8 @@ class Salary {
       `DELETE FROM salaries 
             WHERE id = $1 
             RETURNING id`,
-      [id]);
+      [id]
+    );
 
     if (result.rows.length === 0) {
       let notFound = new Error(`There exists no salary ${id}`);
