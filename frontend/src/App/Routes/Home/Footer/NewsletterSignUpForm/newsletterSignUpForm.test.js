7cc2cf7e59dff442afeab9a1b911f7b769d1ebf7
JonathanCLin <52088996+JonathanCLin@users.noreply.github.com>
2019-10-28 10:11:51 -0700

    R13 footer (#143)
    * Created Footer WIP
    
    Created footer with newletter email form, social media links and logo. Expect logo to be updated. Social media links currently go to ask an expert as placeholder. Need to fix up css.
    
    * Created Footer Frontend and Backend WIP
    
    Created footer component, routes, and DB. Currently working on tests
    
    * Created Footer Frontend and Backend
    
    Finished component, API, routes, and DB. Testing is a little odd, but we decided to move on for now as we will be refactoring later.
    
    * Ran Tests and Added Snapshots
    
    Resolved merge conflict of missing snapshot in Home folder
    
    * Reran Tests to Update Snapshot
    
    Snapshot was failing so ran tests again to update.
    
    * Fixed Footer Test
    
    Fixed test to only find footer so that we snapshots will not run into router history issue
    
    * Created Footer Backend Tests and Existing Email Catch
    
    Wrote backend tests for footer newsletter sign up post and wrote a catch for if an email already exists in the database
    
    * Created Footer Backend Tests and Existing Email Catch
    
    Wrote backend tests for footer newsletter sign up post and wrote a catch for if an email already exists in the database
    
    * Improved Newsletter Routes and Added Tests
    
    Added a find single email function and added tests
    
    * Completed all Change Requests Except One
    
    Test for frontend Newsletter Form is still wonky, but everything else has been addressed
    
    * Completed all Change Requests Except One
    
    Test for frontend Newsletter Form is still wonky, but everything else has been addressed
    
    * Added Last Test
    
    * Added Last Test
    
    * Fixed Backend  Newsletter Test, Frontend Newsletter Form and elevateAPI
    
    * Updated Footer CSS
    
    * Updated Footer CSS
    
    * add padding above footer

---
+++ frontend/src/App/Routes/Home/Footer/NewsletterSignUpForm/newsletterSignUpForm.test.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { shallow, mount } from "enzyme";
+import toJson from "enzyme-to-json";
+import NewsletterSignUpForm from "./newsletterSignUpForm";
+
+describe("NewsletterSignUpForm", function() {
+  it("renders", () => {
+    shallow(<NewsletterSignUpForm />);
+  });
+
+  it("matches snapshot", () => {
+    let wrapper = mount(<NewsletterSignUpForm />);
+    const serialized = toJson(wrapper);
+    expect(serialized).toMatchSnapshot();
+  });
+
+  it("calls onSubmit prop function when form is submitted", () => {
+    const wrapper = mount(<NewsletterSignUpForm />);
+    const instance = wrapper.instance();
+    jest.spyOn(instance, "handleSubmit");
+    instance.handleSubmit();
+
+    expect(instance.handleSubmit).toHaveBeenCalled();
+  });
+});
---
+++ frontend/src/App/Routes/Home/Footer/footer.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import { Link } from "react-router-dom";
+import "./footer.css";
+import NewsletterSignUpForm from "./NewsletterSignUpForm/newsletterSignUpForm";
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <div className="Footer">
+        <Container fluid={true}>
+          <Row>
+            <Col className="d-flex flex-column justify-content-center">
+              <NewsletterSignUpForm />
+            </Col>
+            <Col style={{ display: "flex", flexDirection: "column" }}>
+              <Link to="/ask-an-expert">Blog</Link>
+              <Link to="/ask-an-expert">Contact</Link>
+              <Link to="/ask-an-expert">Facebook</Link>
+              <Link to="/ask-an-expert">Instagram</Link>
+              <Link to="/ask-an-expert">Twitter</Link>
+            </Col>
+            <Col className="d-flex align-items-center justify-content-end">
+              <Link className="brand-logo" to="/">
+                Brella
+              </Link>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default Footer;
