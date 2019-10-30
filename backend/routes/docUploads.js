aeb9fbc2b5a0cdae9eecd7c682c5d15eebef0a09
Jonathan Lin <jonathanlin@LAPTOP-LH80P623.localdomain>
2019-10-30 11:47:50 -0700

    Fixed Public Read Issue

--- backend/routes/docUploads.js
+++ backend/routes/docUploads.js
@@ -32,6 +32,7 @@ const upload = multer({
   storage: multerS3({
     s3: s3,
     bucket: BUCKET_NAME,
+    acl: 'public-read',
     key: function(req, file, cb) {
       cb(null, file.originalname);
     }
