e2afa822fa04b3ac4b8649aff86b2626b8f4bbc4
Nalipp <nateplipp@gmail.com>
2019-11-21 16:29:37 -0800

    re-commit userDocsDataTable

--- frontend/src/App/Routes/Dashboard/dashboardManage/userDocsDataTable.js
+++ frontend/src/App/Routes/Dashboard/dashboardManage/userDocsDataTable.js
@@ -1,29 +1,45 @@
 import React, { Component } from "react";
-import "./userDocsDataTable.css";
-import { Table } from "react-bootstrap";
+import { Table } from "reactstrap";
 
 class UserDocsDataTable extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userDocuments: [
+        {
+          id: "1",
+          document: "Tinkerbell's assistant",
+          counterParty: "Wendy",
+          status: "received",
+          dateReviewed: "-"
+        },
+        {
+          id: "2",
+          document: "Capain Hook's assistant",
+          counterParty: "Peter",
+          status: "pending",
+          dateReviewed: "-"
+        },
+        {
+          id: "3",
+          document: "Peter Pan's assistant",
+          counterParty: "John",
+          status: "completed",
+          dateReviewed: "01/01/2020"
+        }
+      ]
+    };
+  }
+
   renderUserDocsData() {
-    let { documents } = this.props;
-    return documents.map(doc => {
-      const {
-        id,
-        title,
-        counterparty,
-        status,
-        url,
-        date_reviewed,
-        date_submitted
-      } = doc;
+    return this.state.userDocuments.map(doc => {
+      const { id, document, counterParty, status, dateReviewed } = doc;
       return (
         <tr key={id}>
-          <th style={{ width: "450px" }} scope="row">
-            <a href={"http://" + url}>{title}</a>
-          </th>
-          <td>{counterparty}</td>
+          <th scope="row">{document}</th>
+          <td>{counterParty}</td>
           <td>{status}</td>
-          <td>{date_submitted}</td>
-          <td>{date_reviewed}</td>
+          <td>{dateReviewed}</td>
         </tr>
       );
     });
@@ -31,19 +47,18 @@ class UserDocsDataTable extends Component {
 
   render() {
     return (
-      <div className="Documents_table container-fluid">
+      <div className="container">
         <h1>My Documents</h1>
-        <Table hover bordered>
+        <Table striped bordered>
           <thead>
             <tr>
               <th>Document</th>
               <th>Counterparty</th>
               <th>Status</th>
-              <th>Date Submitted</th>
               <th>Date Reviewed</th>
             </tr>
           </thead>
-          {<tbody>{this.renderUserDocsData()}</tbody>}
+          <tbody>{this.renderUserDocsData()}</tbody>
         </Table>
       </div>
     );
