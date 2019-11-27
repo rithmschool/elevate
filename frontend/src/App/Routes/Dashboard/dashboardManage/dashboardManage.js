9279a94cf293d9acb8f618c65a7d33ada28f1e0e
Christian Zenaty <cazenaty@yahoo.com>
2019-11-26 17:12:19 -0800

    Fix for duplicate naming issue

--- frontend/src/App/Routes/Dashboard/dashboardManage/dashboardManage.js
+++ frontend/src/App/Routes/Dashboard/dashboardManage/dashboardManage.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import UserDocsDataTable from "./userDocsDataTable";
+import UserDocsDataTable from "./xuserDocsDataTable";
 import ElevateApi from "../../../../elevateApi";
 import Spinner from "../../../Spinner/spinner";
 import UserDocUploads from "./UserDocUploads";
